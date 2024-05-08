678d2f783889f072d13368ed7688271c51797cd5
Vladimir <piterskivv@mail.ru>
2024-05-08 22:39:43 +0400

    подключил allure

--- src/test/java/ru/vladimir/SelenideTest.java
+++ src/test/java/ru/vladimir/SelenideTest.java
@@ -1,2 +1,26 @@
-package ru.vladimir;public class SelenideTest {
+package ru.vladimir;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class SelenideTest {
+
+    @Test
+    public void testIssueSearch(){
+        Configuration.holdBrowserOpen = true;
+        open("https://github.com/");
+        $("[data-target='qbsearch-input.inputButtonText']").click();
+        $("[name='query-builder-test']").sendKeys("eroshenkoam/allure-example");
+        $("[name='query-builder-test']").submit();
+        $(linkText("eroshenkoam/allure-example")).click();  // поиск текста и клик по нему
+        $("#issues-tab").click();
+        $(withText("#87")).should(Condition.exist);   // проверка по тексту
+    }
 }
